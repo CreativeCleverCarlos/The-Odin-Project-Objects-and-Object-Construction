81f532d5de297e7aeba63fb3737a22be2e82de36
Carlos Campbell <carlos.g.campbell@gmail.com>
2024-02-09 14:32:27 -0500

    more notes, Feb 9th

--- main.js
+++ main.js
@@ -69,3 +69,20 @@ player2.sayName(); //logs 'Jane'
 Write a constructor for making “Book” objects. We will revisit this in the project at the end of this lesson. Your book objects should have the book’s title, author, the number of pages, and whether or not you have read the book. 
 
 **/
+
+function Book(title, author, pages, read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function(){
+        return `${title}, ${author}, ${pages}, ${read}` //back ticks are called template literals
+    };
+}
+
+const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'not read yet');
+
+console.log(theHobbit.info());
+
+
+//Alright, and unto protoypes I go.
